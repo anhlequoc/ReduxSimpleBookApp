72e7b1da44d11c31779da2a6b781e5fc5f223a23
Anh Le <lequocanh88@gmail.com>
2017-06-02 18:23:54 +0700

    complete section 4

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList () {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
-      )
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
+      );
     });
   }
-  render () {
 
+  render () {
     return (
       <ul classsName="list-group col-sm-4">
         {this.renderList()}
@@ -31,8 +38,16 @@ function mapStateToProps (state) { //take applicate state - contain array of boo
     }
 }
 
+
+//anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps (dispatch) {
+  // Whenever selectBook is called, the result should be passed to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch); //có thể gọi this.props.selectBook
+}
+
+//Promote BookList from a component to a container - it needs to know about this new dispatch method, selectBook. Make it available as a prop.
 //connect function: take this component, take this mapStateToProps and return a container component
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
 //important notes:
 // - when using connect(), whenever the state changes, the component, container is auto re-render with the new list of books
 // - Whenever state changes, the object in state function will be assigned as props in component
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function () {
   return [
-    {title: "book 1"},
-    {title: "book 2"},
-    {title: "book 3"},
-    {title: "book 4"},
-    {title: "book 5"}
+    {title: "book 1", pages: 200},
+    {title: "book 2", pages: 300},
+    {title: "book 3", pages: 2123},
+    {title: "book 4", pages: 414},
+    {title: "book 5", pages: 556}
   ]
 }
